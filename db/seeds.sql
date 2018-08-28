376e2f5c726957b99736ded3366533b286081017
Jacob Henderson <Hendersonjacob1492@gmail.com>
2018-08-28 08:38:35 -0400

    Created the Config Folder with Connection.js and orm.js files completed.

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Mushroom');
+INSERT INTO burgers (burger_name) VALUES ('Bacon & Swiss');
+INSERT INTO burgers (burger_name) VALUES ('Classic');
+INSERT INTO burgers (burger_name) VALUES ('Double Stack');
